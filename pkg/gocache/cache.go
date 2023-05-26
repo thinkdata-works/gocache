ecd37ccc9212ca3ea230839b3401eb33227ddbd8
Chris Sandison <chris@thinkdataworks.com>
2023-05-26 09:55:24 +0200

    Removing keygen from responsibilities

--- pkg/gocache/cache.go
+++ pkg/gocache/cache.go
@@ -6,35 +6,24 @@ import (
 	"github.com/thinkdata-works/gopromise/pkg/promise"
 )
 
-type Cache[K any, V any] struct {
-	generateKey func(K) (string, error)
-	cache       *partitionedCache[string, promise.Promise[V]]
+type Cache[K comparable, V any] struct {
+	cache *partitionedCache[K, promise.Promise[V]]
 }
 
-func NewCache[K any, V any](partitionSize, totalPartitions int, cacheExpiry time.Duration, keygen func(K) (string, error)) *Cache[K, V] {
+func NewCache[K comparable, V any](partitionSize, totalPartitions int, cacheExpiry time.Duration) *Cache[K, V] {
 	return &Cache[K, V]{
-		cache: newPartitionedCached[string, promise.Promise[V]](
+		cache: newPartitionedCached[K, promise.Promise[V]](
 			partitionSize, totalPartitions, cacheExpiry,
 		),
-		generateKey: keygen,
 	}
 }
 
 func (c *Cache[K, V]) HasKey(k K) (bool, error) {
-	key, err := c.generateKey(k)
-	if err != nil {
-		return false, err
-	}
-	return c.cache.HasKey(key), nil
+	return c.cache.HasKey(k), nil
 }
 
 func (c *Cache[K, V]) Get(k K, getter func(K) (*V, error)) (*V, error) {
-	key, err := c.generateKey(k)
-	if err != nil {
-		return getter(k)
-	}
-
-	valpromise, alreadyExists := c.cache.GetOrCreate(key, promise.NewPromise[V]())
+	valpromise, alreadyExists := c.cache.GetOrCreate(k, promise.NewPromise[V]())
 	if alreadyExists {
 		val, err := valpromise.Wait()
 		if err != nil {
